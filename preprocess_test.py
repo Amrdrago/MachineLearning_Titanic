69ace7c12355833226fd64770f5c40c9740aefc7
Prankster10 <loaysalah276@gmail.com>
2024-05-10 17:53:57 +0300

    Updates

--- preprocess_test.py
+++ preprocess_test.py
@@ -8,8 +8,7 @@ df_test = pd.read_csv("test.csv")
 df_test.drop(['name', 'cabin', 'ticket'], axis=1, inplace=True)
 df_test['sex'] = df_test['sex'].map({'male': 0, 'female': 1})
 
-# Handling missing values
-print(df_test.isnull().sum())
+
 
 # Impute missing values for numerical variables
 df_test['age'].fillna(df_test['age'].median(), inplace=True)
@@ -22,6 +21,9 @@ df_test['age'] = winsorize(df_test['age'], limits=[0.05, 0.05])
 df_test.dropna(subset=['age'], inplace=True)
 scaler = MinMaxScaler()
 df_test = pd.DataFrame(scaler.fit_transform(df_test), columns=df_test.columns)
+
+# Handling missing values
+print(df_test.isnull().sum())
 
 # Compute the correlation matrix using numeric columns only
 correlation_matrix = df_test.select_dtypes(include=[np.number]).corr()
