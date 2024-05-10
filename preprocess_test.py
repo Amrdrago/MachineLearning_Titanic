323e202d728c7f5a4830c5a83f477ac7fa1ae32f
Abdelrahman Malavi <Abdelrahman2k16@gmail.com>
2024-05-10 15:46:56 +0300

    This is the testing dataset

---
+++ preprocess_test.py
@@ -0,0 +1,28 @@
+import numpy as np
+from scipy.stats._mstats_basic import winsorize
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+df_test = pd.read_csv("test.csv")
+
+df_test.drop(['name', 'cabin', 'ticket'], axis=1, inplace=True)
+df_test['sex'] = df_test['sex'].map({'male': 0, 'female': 1})
+
+# Handling missing values
+print(df_test.isnull().sum())
+
+# Impute missing values for numerical variables
+df_test['age'].fillna(df_test['age'].median(), inplace=True)
+
+# Encode categorical variables
+df_test = pd.get_dummies(df_test, columns=['embarked'], drop_first=True)
+
+# Remove outliers using Winsorization
+df_test['age'] = winsorize(df_test['age'], limits=[0.05, 0.05])
+df_test.dropna(subset=['age'], inplace=True)
+scaler = MinMaxScaler()
+df_test = pd.DataFrame(scaler.fit_transform(df_test), columns=df_test.columns)
+
+# Compute the correlation matrix using numeric columns only
+correlation_matrix = df_test.select_dtypes(include=[np.number]).corr()
+df_test.to_csv("titanic_preprocessed_test.csv", index=False)
