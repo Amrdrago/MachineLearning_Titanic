d93fa2e63b0c01e929af7a2c3ef34fe64fbbaf01
Loay Salah <loaysalah276@gmail.com>
2024-05-10 19:33:32 +0300

    naive_bayes.py

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,10 +1,10 @@
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import accuracy_score, f1_score
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 import pandas as pd
 
 
-def train(dataframe, classifier):
+def train(dataframe, classifier) :
     # Split data into features and labels
     X = dataframe.drop(['survived'], axis=1)  # Features
     y = dataframe['survived']  # Labels
@@ -26,6 +26,13 @@ def train(dataframe, classifier):
     accuracy_test = accuracy_score(y_test, y_pred_test)
     accuracy_train = accuracy_score(y_train, y_pred_train)
 
+    # Calculate precision
+    precision_test = precision_score(y_test, y_pred_test)
+    precision_train = precision_score(y_train, y_pred_train)
+
+    # Calculate recall
+    recall_test = recall_score(y_test, y_pred_test)
+    recall_train = recall_score(y_train, y_pred_train)
     # Calculate F1 score
     f1_test = f1_score(y_test, y_pred_test)
     f1_train = f1_score(y_train, y_pred_train)
@@ -33,18 +40,24 @@ def train(dataframe, classifier):
     print("Accuracy")
     print("Test: {:.2f}%".format(accuracy_test * 100))
     print("Train: {:.2f}%".format(accuracy_train * 100), '\n')
+    print("Precision")
+    print("Test:", precision_test)
+    print("Train:", precision_train, '\n')
+    print("Recall")
+    print("Test:", recall_test)
+    print("Train:", recall_train, '\n')
     print("F1 Score")
     print("Test:", f1_test)
     print("Train:", f1_train)
 
 
-def prediction(dataframe, classifier):
+def prediction(dataframe, classifier) :
     nb_classifier = classifier
     predicts = nb_classifier.predict(dataframe)
     return predicts
 
 
-if __name__ == '__main__':
+if __name__ == '__main__' :
     # Load the dataframes ------------------------------
     df = pd.read_csv("titanic_preprocessed.csv")
     df_predict = pd.read_csv("titanic_test_preprocessed.csv")
