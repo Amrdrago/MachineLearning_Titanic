4a2a7c363ae40218e92b1485c3b86c0e268e759c
Abdelrahman Malavi <Abdelrahman2k16@gmail.com>
2024-05-10 16:32:08 +0300

    This is the testing dataset

--- naive_bayes.py
+++ naive_bayes.py
@@ -12,11 +12,11 @@ df.dropna(subset=["fare"], inplace=True)
 # Replace 'df' with your actual DataFrame name
 
 # Split data into features and labels
-X = df.drop(['pclass', 'sex', 'age', 'sibsp', 'parch', 'fare', 'embarked_Q', 'embarked_S'], axis=1)  # Features
+X = df.drop(['pclass', 'sex', 'age', 'sibsp', 'parch', 'fare'], axis=1)  # Features
 y = df['survived']  # Labels
 
 # Split data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=123,stratify=y)
 
 # Initialize Gaussian Naive Bayes classifier
 nb_classifier = GaussianNB()
@@ -34,38 +34,13 @@ accuracy_train = accuracy_score(y_train, y_pred_train)
 
 print("Test Accuracy:", accuracy_test * 100, "%")
 print("Train Accuracy:", accuracy_train * 100, "%")
-# TRAIN
-df_test = pd.read_csv("test.csv")
-
-df_test.drop(['name', 'cabin', 'ticket'], axis=1, inplace=True)
-df_test['sex'] = df_test['sex'].map({'male': 0, 'female': 1})
-
-# Handling missing values
-print(df_test.isnull().sum())
-
-# Impute missing values for numerical variables
-df_test['age'].fillna(df_test['age'].median(), inplace=True)
-
-# Encode categorical variables
-df_test = pd.get_dummies(df_test, columns=['embarked'], drop_first=True)
-
-# Remove outliers using Winsorization
-df_test['age'] = winsorize(df_test['age'], limits=[0.05, 0.05])
-df_test.dropna(subset=['age'], inplace=True)
-scaler = MinMaxScaler()
-df_test = pd.DataFrame(scaler.fit_transform(df_test), columns=df_test.columns)
-
-# Compute the correlation matrix using numeric columns only
-correlation_matrix = df_test.select_dtypes(include=[np.number]).corr()
-df_test.to_csv("titanic_preprocessed_test.csv", index=False)
 
 df_predict = pd.read_csv("titanic_preprocessed_test.csv")
 # Add placeholder values for the 'survived' column
+X = df_predict.drop(['pclass', 'sex', 'age', 'sibsp', 'parch', 'fare'], axis=1)
 
-# Reorder columns to match the order during training
-df_predict = df_predict[X.columns]
 # Make predictions using the trained Naive Bayes classifier
-predictions = nb_classifier.predict(df_predict)
-
+predictions = nb_classifier.predict(X)
+df_predict['survived'] = predictions
 # Print the predictions
-print(predictions)
+print(df_predict.to_string())
